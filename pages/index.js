8f6627e56adeeef818ff7060ad1ba07937648172
PrettyLolli <yunghunters2015@gmail.com>
2023-04-29 04:23:08 +0300

    попап на картинки работает, осталась кнопка закрытия

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-const openPopupBtn = document.querySelector(".profile__edit-button_popup_opened");
+const openPopupBtn = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseBtn = document.querySelector(".popup__form-close-btn");
 const popupSaveBtn = document.querySelector(".popup__form-button-save");
@@ -47,56 +47,6 @@ formSave.addEventListener("click", closePopupByClick);
 
 
 
-/* Поп ап для добавления картинок */
-
-const PopupAddBtn = document.querySelector(".profile__add-button_popup_added");
-const popupNameAdd = document.querySelector(".popup__text");
-const popupAdd = document.querySelector(".popup__add");
-
-
-function openPopupAdd() {
-    popupNameAdd.value = cardsTitle.textContent;
-    popupAdd.classList.add("popup_added");
-
-}
-function closePopupAddByClick(evt) {
-    let isOverlay = evt.target.classList.contains("popup__add");
-    let isCloseBtn = evt.target.classList.contains("popup__form-close-btn");
-    if (isOverlay || isCloseBtn) {
-        closePopupAdd();
-    }
-}
-function closePopupAdd() {
-    popupAdd.classList.remove("popup_added");
-}
-
-function formSubmitHandlerAdd(evt) {
-    evt.preventDefault();
-    
-    closePopup();
-}
-
-PopupAddBtn.addEventListener("click", openPopupAdd);
-popupCloseBtn.addEventListener("click", closePopupAdd);
-popupAdd.addEventListener("click", closePopupAddByClick);
-formElement.addEventListener("submit", formSubmitHandlerAdd);
-formSave.addEventListener("click", closePopupAddByClick);
-
-
-
-
-/* Поп ап для изображений
-
-const  popupImages = document.querySelector('.popup__images');
-const popupPicture = document.querySelector('.popup__picture');
-const popupPictureDesc = document.querySelector('.popup__picture-description');
-*/
-
-
-
-
-
-
 
 
 
@@ -108,6 +58,7 @@ const initialCards = [
     {
         name: 'Капибара отдыхает',
         link: 'https://im.wampi.ru/2023/04/24/Capybara-in-the-pool.jpg'
+        
     },
     {
         name: 'Капибара работает',
@@ -143,29 +94,165 @@ const createCardElement = (cardsData) => {
     const cardsTitle = cardsElement.querySelector('.groups__text');
     const cardsDeleteBtn = cardsElement.querySelector('.groups__deletebtn');
     const cardsLikeBtn = cardsElement.querySelector('.groups__like');
-
-
+    
 
     cardsTitle.textContent = cardsData.name;
     cardsImage.src = cardsData.link;
-    cardsImage.alt = cardsData.name;
 
-    const handleDelete = (evt) => {
+    const handleDelete = () => {
         cardsElement.remove();
-    }
+    };
 
     const handleLike = (evt) => {
         evt.target.classList.toggle('groups__like_active');
-    }
+    };
 
+    function openPopupImg (){
+
+        popupDescription.textContent = cardsData.name;
+        popupImages.src = cardsData.link;
+    
+        popupImg.classList.add('popup_opened');
+    }
+    
+    
+
+    cardsImage.addEventListener('click',openPopupImg);
     cardsDeleteBtn.addEventListener('click', handleDelete);
     cardsLikeBtn.addEventListener('click', handleLike);
-
+   
     return cardsElement;
 };
 
-initialCards.forEach((card) => {
-    const element = createCardElement(card);
-    groupsContainer.append(element);
+
+const renderAddElement = (cardsElement) => {
+    groupsContainer.prepend(cardsElement)
+}
+
+
+initialCards.forEach((cards) => {
+    const element = createCardElement(cards);
+    renderAddElement(element);
+    
 });
+
+
+
+
+
+/* Поп ап для добавления картинок */
+
+const PopupAddBtn = document.querySelector(".profile__add-button");
+const popupAdd = document.querySelector(".popup_type_add");
+const popupAddForm = document.querySelector('.popup__form-add');
+const popupNameAdd = document.querySelector(".popup__text");
+const popupLinkAdd = document.querySelector('.popup__link');
+
+
+function openPopupAdd() {
+    popupAdd.classList.add("popup_opened");
+
+}
+function closePopupAddByClick(evt) {
+    let isOverlay = evt.target.classList.contains("popup_type_add");
+    let isCloseBtn = evt.target.classList.contains("popup__form-close-btn");
+    if (isOverlay || isCloseBtn) {
+        closePopupAdd();
+    }
+}
+function closePopupAdd() {
+    popupAdd.classList.remove("popup_opened");
+}
+
+const handleAddFormSubmit  = (event) => { 
+    event.preventDefault();
+
+    const  name = popupNameAdd.value;
+    const link = popupLinkAdd.value ;
+
+    const initialCards = {
+    name,
+    link,
+    };
+
+    renderAddElement(createCardElement(initialCards));
+    closePopupAdd(popupAdd);
+};
+
+
+PopupAddBtn.addEventListener("click", openPopupAdd);
+popupCloseBtn.addEventListener("click", closePopupAdd);
+popupAdd.addEventListener("click", closePopupAddByClick);
+popupAddForm.addEventListener("submit", handleAddFormSubmit);
+formSave.addEventListener("click", closePopupAddByClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Поп ап для фоток */
+const popupImg = document.querySelector('.popup_type_img');
+const popupImages = document.querySelector('.popup__picture');
+const popupDescription = document.querySelector('.popup__picture-description');
+const popupImgClose = document.querySelector('.popup__images-closebtn');
+const popupImgForm = document.querySelector('.popup__images-container');
+
+
+
+function closePopupImgByClick(evt) {
+    let isOverlay = evt.target.classList.contains("popup_type_img");
+    let isCloseBtn = evt.target.classList.contains("popup__images-closebtn");
+    if (isOverlay || isCloseBtn) {
+        closePopupImg();
+    }
+}
+function closePopupImg() {
+    popupImg.classList.remove("popup_opened");
+}
+
+const handleImgClick = (cardsData) => { 
+    cardsData.preventDefault();
+    cardsData.name = popupDescription.textContent;
+    cardsData.link = popupImages.src;
+}
+
+
+popupImgClose.addEventListener("click", closePopupImg);
+popupImg.addEventListener("click", closePopupImgByClick);
+popupImgForm.addEventListener("submit",handleImgClick);
+
+/*
+const cardsImage = cardsElement.querySelector('.groups__image');
+cardsImage.forEach((groupsImg)) => {
+    groupImage.addEventListener('click', () => {
+        popupImages.src = groupsImg.link;
+        popupDescription.textContent = groupsImg.name;
+    });
+}
+
+initialCards.forEach((cards) => {
+    const element = createCardElement(cards);
+    renderAddElement(element);
+    
+});
+
+
+const renderAddElement = (cardsElement) => {
+    groupsContainer.prepend(cardsElement)
+}
+
+ popupImages.src = cardsData.link;
+    popupDescription.textContent = cardsData.name;
+*/
 
